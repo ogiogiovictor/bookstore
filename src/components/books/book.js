f93448857105a0c9423891d05b443e182dc445d9
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-07-16 03:17:40 -0600

    fixed eslint issues

--- src/components/books/book.js
+++ src/components/books/book.js
@@ -1,29 +1,18 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import SingleBook from './singlebook';
 
 const Book = (props) => (
   <div>
-    {
-                props.books.map((bk) => (
-                  <SingleBook
-                    key={bk.id}
-                    title={bk.title}
-                    category={bk.category}
-                    author={bk.author}
-                    status={bk.status}
-                  />
-                ))
-            }
-
+    {props.books.map((bk) => (
+      <SingleBook
+        key={bk.id}
+        title={bk.title}
+        category={bk.category}
+        author={bk.author}
+        status={bk.status}
+      />
+    ))}
   </div>
 );
-
-// Book.propTypes = {
-//   title: PropTypes.string.isRequired,
-//   category: PropTypes.string.isRequired,
-//   author: PropTypes.string.isRequired,
-//   status: PropTypes.bool.isRequired,
-// };
 
 export default Book;
